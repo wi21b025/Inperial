d7ba30c2c991b0577da7a84704ffef85a896a380
wi21b025 <wi21b025@technikum-wien.at>
2022-06-22 14:00:44 +0200

    Initial commit

---
+++ src/main/java/com/example/inperial/InperialApplication.java
@@ -0,0 +1,13 @@
+package com.example.inperial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InperialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InperialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inperial/InperialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inperial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InperialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
